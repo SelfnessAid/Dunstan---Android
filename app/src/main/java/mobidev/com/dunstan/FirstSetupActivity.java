c408585870de8fddbfa0421979d195cc651dbfd5
SelfnessAid <daxianjin59@hotmail.com>
2017-05-02 07:31:20 +0300

    implement the splash screen

---
+++ app/src/main/java/mobidev/com/dunstan/FirstSetupActivity.java
@@ -0,0 +1,13 @@
+package mobidev.com.dunstan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_setup);
+    }
+}
